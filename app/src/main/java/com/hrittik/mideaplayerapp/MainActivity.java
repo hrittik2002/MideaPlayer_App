3f540f0f762b307c6883d11c61a4ae6945136507
Hrittik <hrittikgarain@gmail.com>
2021-06-14 17:35:57 +0530

    This is my first commit

---
+++ app/src/main/java/com/hrittik/mideaplayerapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.hrittik.mideaplayerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaParser;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer player;
+    public void play(View view){
+        player.start();
+    }
+    public void pause(View view){
+        player.pause();
+    }
+    public void pstop(View view){
+        player.stop();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        player = MediaPlayer.create(this , R.raw.music);
+    }
+}
